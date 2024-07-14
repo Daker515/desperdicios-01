bd2adf8e8a26d781d56e17c9e687da42a5e73e7f
Daker515 <erosjejexd3@gmail.com>
2024-07-13 23:59:43 -0500

    commit 02

--- src/main/java/com/foodappbackend/foodapp/FoodAppApplication.java
+++ src/main/java/com/foodappbackend/foodapp/FoodAppApplication.java
@@ -6,22 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(FoodAppApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
-
+	
 }
